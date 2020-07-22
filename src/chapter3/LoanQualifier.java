2792bcc65d31ec10706364f59c1b220f96142b98
motovmp1 <vinicius.mpinho@gmail.com>
2020-07-22 13:46:33 +0800

    Running Test 3

--- src/chapter3/LoanQualifier.java
+++ src/chapter3/LoanQualifier.java
@@ -1,10 +1,41 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class LoanQualifier {
 
     public static void main(String[] args)
     {
+    // Initialize what we know
+        int requiredSalary = 30000;
+        int requiredYearsEmploy = 2;
 
+        // Get what we don't
+        System.out.println("Enter the salary: ");
+        Scanner scanner = new Scanner(System.in);
+        double salary = scanner.nextDouble();
+
+        System.out.println("Enter the number os year with you current employee: ");
+        double years = scanner.nextDouble();
+        scanner.close();
+
+        //Make decision
+        if(salary >= requiredSalary)
+        {
+            if(years >= requiredYearsEmploy)
+            {
+                System.out.println("Congrats! you are the best");
+            }
+            else
+            {
+                System.out.println("Sorry you must have worler at your curent job " + requiredYearsEmploy);
+            }
+
+        }
+        else
+        {
+          System.out.println("Sorry, you must ear at least $" + requiredSalary + " This year");
+        }
 
 
 
